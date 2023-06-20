db8e48f3b75ac53b1a7620ce2fa8d56bb79e2486
Ahmed Abasiyanik <aabasiyanik@Ahmeds-MacBook-Pro.local>
2023-06-19 22:51:57 -0500

    fixed the issues, now improving the user experience

--- hangman.py
+++ hangman.py
@@ -65,17 +65,19 @@ word = ["_"] * len(choosen_word)
 end = False
 
 while not end:
+    print(f"{' '.join(word)}")
     guess = input("Guess a letter: ").lower()
     for index in range(len(choosen_word)):
         letter = choosen_word[index]
         if guess == letter:
             word[index] = guess
-        else:
-            pass
+        elif guess not in choosen_word:
+            print(stages[chances])
+            chances -= 1
+            break
     if "_" not in word:
         print("You win")
         end = True
-    elif chances == 0:
+    elif chances < 0:
         print("You lost")
-
-print(word)
+        end = True
