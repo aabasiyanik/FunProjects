3f5c77cdd1a685858953f25294a2751047c2b88f
Ahmed Abasiyanik <aabasiyanik@Ahmeds-MacBook-Pro.local>
2023-06-19 22:39:50 -0500

    trying to solve how to chances -= 1 if the word is not within the string

--- hangman.py
+++ hangman.py
@@ -1,13 +1,81 @@
 import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+chances = 6
+
 word_list = ["elephant", "banana", "sunflower", "giraffe", "piano", "chocolate", "mountain", "umbrella", "watermelon", "butterfly", "sapphire", "pineapple", "dragonfly", "peacock", "moonlight", "cappuccino", "whisper", "jigsaw", "velvet", "tornado"]
 choosen_word = random.choice(word_list)
 print(choosen_word)
-guess = input("Guess a letter: ").lower()
 word = ["_"] * len(choosen_word)
+end = False
 
-for index in range(len(choosen_word)):
-    letter = choosen_word[index]
-    if guess == letter:
-        word[index] = guess
+while not end:
+    guess = input("Guess a letter: ").lower()
+    for index in range(len(choosen_word)):
+        letter = choosen_word[index]
+        if guess == letter:
+            word[index] = guess
+        else:
+            pass
+    if "_" not in word:
+        print("You win")
+        end = True
+    elif chances == 0:
+        print("You lost")
 
 print(word)
