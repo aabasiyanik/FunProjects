e464a95c2a6d4b40b764ff06a3909e7f13121fd7
Ahmed Abasiyanik <ahmedabasiyanik03@gmail.com>
2023-06-29 02:13:12 -0500

    mostly done with the game, added clear() method and also fixed the bugs when draw ace

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,10 @@
 import random
-cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+from os import system, name
+def clear():
+   if name == 'nt':
+      _ = system('cls')
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 def draw_card():
     draw = random.choice(cards)
@@ -9,6 +14,7 @@ def draw_card():
 def blackjack():
     play_or_no = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
     if play_or_no == 'y':
+        clear()
         pass
     else:
         exit()
@@ -27,11 +33,20 @@ def blackjack():
         ans = input("Type 'y' to get another card, type 'n' to pass: ")
         if ans == 'y':
             user.append(draw_card())
-            if sum(user) > 21:
-                print(f"\tYour final hand: {user}, final score: {sum(user)}\n\tComputer's final hand: {computer}, final score: {sum(computer)}\nYour went over. You lose")
-                blackjack()
-            elif sum(user) == 21:
+            if sum(user) == 21:
                 print(f"\tYour final hand: {user}, final score: {sum(user)}\n\tComputer's final hand: {computer}, final score: {sum(computer)}\nYou Win with Blackjack")
+                blackjack()
+            elif sum(user) > 21:
+                if 11 in user and sum(user) > 21:
+                    aces = user.count(11)
+                    while aces > 0 and sum(user) > 21:
+                        user[user.index(11)] = 1
+                        aces -= 1
+                if sum(user) > 21:
+                    print(f"\tYour final hand: {user}, final score: {sum(user)}\n\tComputer's final hand: {computer}, final score: {sum(computer)}\nYou went over. You lose.")
+                    blackjack()
+                else:
+                    pass
             else:
                 pass
         else:
@@ -40,11 +55,17 @@ def blackjack():
     while sum(computer) < 17:
         computer.append(draw_card())
     
+        if 11 in computer and sum(computer) >= 17 and sum(computer) > 21:
+            aces = computer.count(11)
+            while aces > 0 and sum(computer) > 21:
+                computer[computer.index(11)] = 1
+                aces -= 1
+
     if sum(computer) > 21:
         print(f"\tYour final hand: {user}, final score: {sum(user)}\n\tComputer's final hand: {computer}, final score: {sum(computer)}\nYou Win")
         blackjack()
     elif sum(computer) == 21:
-        print(f"\tYour final hand: {user}, final score: {sum(user)}\n\tComputer's final hand: {computer}, final score: {sum(computer)}\Computer Win with Blackjack")
+        print(f"\tYour final hand: {user}, final score: {sum(user)}\n\tComputer's final hand: {computer}, final score: {sum(computer)}\nComputer Win with Blackjack")
         blackjack()
     elif sum(computer) > sum(user):
         print(print(f"\tYour final hand: {user}, final score: {sum(user)}\n\tComputer's final hand: {computer}, final score: {sum(computer)}\nYou Lose"))
