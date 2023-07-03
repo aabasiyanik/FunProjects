173c705499a468674632280e800af173218473d4
Ahmed Abasiyanik <ahmedabasiyanik03@gmail.com>
2023-07-03 12:53:27 -0500

    adding the algorithm to check if the machine has enough ingredients for the requested coffee

--- coffeeMachine.py
+++ coffeeMachine.py
@@ -32,9 +32,15 @@ resources = {
     "coffee": 100,
 }
 # print(COFFEES['espresso']['ingredients']['water'])
-# def isItEnough(ingredients):
-#     for item in ingredients:
-#         if item <
+def isItEnough(ingredients):
+    for key, value in ingredients.items():
+        if value is not None and resources[key] > value:
+            resources[key] -= value
+        elif value is not None and resources[key] < value:
+            print(f"Sorry there is not enough {key}.")
+            return False
+    return True
+
 
 def coffeeMachine():
     penny = 0.01
@@ -42,26 +48,32 @@ def coffeeMachine():
     dime = 0.1
     quarter = 0.25
 
-    isValid = True
-    while isValid:
-        user_req = input("What would you like? (espresso/latte/cappuccino): ").lower()
-        if user_req not in ['espresso', 'latte', 'cappuccino', 'report', 'off']:
-            print("Invalid Output")
+    isTrue = True
+    while isTrue:
+        isValid = True
+        while isValid:
+            global profit
+            user_req = input("What would you like? (espresso/latte/cappuccino): ").lower()
+            if user_req not in ['espresso', 'latte', 'cappuccino', 'report', 'off']:
+                print("Invalid Output")
+            else:
+                isValid = False
+        if user_req == 'off':
+            exit()
+        elif user_req == 'report':
+            print(f"Water: {resources['water']}ml\nMilk: {resources['milk']}ml\nCoffee: {resources['coffee']}g\nMoney: ${profit}")
         else:
-            isValid = False
-    if user_req == 'off':
-        exit()
-    elif user_req == 'report':
-        print(f"Water: {resources['water']}ml\nMilk: {resources['milk']}ml\nCoffee: {resources['coffee']}g\nMoney: ${profit}")
-    else:
-        usr_water = COFFEES[user_req]['ingredients']['water']
-        if 'milk' in COFFEES[user_req]['ingredients']:
-            usr_milk = COFFEES[user_req]['ingredients']['milk']
-        else:
-            usr_milk = None
-        usr_coffee = COFFEES[user_req]['ingredients']['coffee']
-        price += COFFEES[user_req]['cost']
-    print(usr_milk)
-
+            usr_water = COFFEES[user_req]['ingredients']['water']
+            if 'milk' in COFFEES[user_req]['ingredients']:
+                usr_milk = COFFEES[user_req]['ingredients']['milk']
+            else:
+                usr_milk = None
+            usr_coffee = COFFEES[user_req]['ingredients']['coffee']
+            profit += COFFEES[user_req]['cost']
+            isTrue = False
+    ingredients = {'water': usr_water, 'milk': usr_milk, 'coffee': usr_coffee}
+    isEnough = isItEnough(ingredients=ingredients)
+    
+    
 
 coffeeMachine()
