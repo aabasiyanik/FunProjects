0396200c2fb265c4966a4d6aa56c1c46446460f1
Ahmed Abasiyanik <ahmedabasiyanik03@gmail.com>
2023-07-02 20:47:54 -0500

    final version of the game is ready, fixed some of the bugs, there are still some waiting to be fixed.

--- HigherOrLower/higherOrLower.py
+++ HigherOrLower/higherOrLower.py
@@ -1,4 +1,4 @@
-from data import data
+from data import data, vs_ascii
 import random
 from os import system, name
 
@@ -6,42 +6,42 @@ def clear():
    if name == 'nt':
       _ = system('cls')
 
-print(data[0].keys())
-
+correct = 0
 def higher_lower():
     randomA = random.choice(data)
     randomB = random.choice(data)
-    while randomA != randomB:
-        randomB = random.choice(data)
+    if randomA == randomB:
+        while randomA != randomB:
+            randomB = random.choice(data)
+
     randomA_name = randomA["name"]
     randomA_follower_count = randomA["follower_count"]
-    randomA_description = randomA["name"]
+    randomA_description = randomA["description"]
     randomA_country = randomA["country"]
     
     randomB_name = randomB["name"]
     randomB_follower_count = randomB["follower_count"]
-    randomB_description = randomB["name"]
+    randomB_description = randomB["description"]
     randomB_country = randomB["country"]
-    
-    correct = 0
+    global correct
     correct_ans = max(randomA_follower_count, randomB_follower_count)
 
     print(f"Compare A: {randomA_name}, a {randomA_description}, from {randomA_country}")
-    print(f"Compare A: {randomB_name}, a {randomB_description}, from {randomB_country}")
+    print(vs_ascii)
+    print(f"Compare B: {randomB_name}, a {randomB_description}, from {randomB_country}")
     user_ans = input("Who has more followers? Type 'A' or 'B': ").lower()
-    if user_ans == 'a' and randomA_follower_count > correct_ans:
+    if user_ans == 'a' and randomB_follower_count < correct_ans:
         correct += 1
         clear()
         print(f"You're right! Current score: {correct}.")
         higher_lower()
-    elif user_ans == 'b' and randomB_follower_count > correct_ans:
+    elif user_ans == 'b' and randomA_follower_count < correct_ans:
         correct += 1
         clear()
         print(f"You're right! Current score: {correct}.")
         higher_lower()
     else:
-        clear
+        clear()
         print(f"Sorry. that's wrong. Final score {correct}")
-
 
 higher_lower()
