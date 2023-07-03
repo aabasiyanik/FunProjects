752f1cdafefbe48b497ec4e11b08822b522f1bbc
Ahmed Abasiyanik <ahmedabasiyanik03@gmail.com>
2023-07-02 20:55:02 -0500

    the code is now bug free, all the bugs are fixed and the user can play it.

--- HigherOrLower/higherOrLower.py
+++ HigherOrLower/higherOrLower.py
@@ -10,9 +10,8 @@ correct = 0
 def higher_lower():
     randomA = random.choice(data)
     randomB = random.choice(data)
-    if randomA == randomB:
-        while randomA != randomB:
-            randomB = random.choice(data)
+    while randomA == randomB:
+        randomB = random.choice(data)
 
     randomA_name = randomA["name"]
     randomA_follower_count = randomA["follower_count"]
@@ -26,10 +25,20 @@ def higher_lower():
     global correct
     correct_ans = max(randomA_follower_count, randomB_follower_count)
 
-    print(f"Compare A: {randomA_name}, a {randomA_description}, from {randomA_country}")
-    print(vs_ascii)
-    print(f"Compare B: {randomB_name}, a {randomB_description}, from {randomB_country}")
-    user_ans = input("Who has more followers? Type 'A' or 'B': ").lower()
+    isValid = True
+    while isValid:
+        print(f"Compare A: {randomA_name}, a {randomA_description}, from {randomA_country}")
+        print(vs_ascii)
+        print(f"Compare B: {randomB_name}, a {randomB_description}, from {randomB_country}")
+        user_ans = input("Who has more followers? Type 'A' or 'B': ").lower()
+        if user_ans not in ['a', 'b']:
+            clear()
+            print("Invalid Input!!!")
+            if correct > 0:
+                print(f"Current score: {correct}\n")
+        else:
+            isValid = False
+
     if user_ans == 'a' and randomB_follower_count < correct_ans:
         correct += 1
         clear()
