e4390a0938aad2de22ff3518d963ae61da8d8248
Ahmed Abasiyanik <ahmedabasiyanik03@gmail.com>
2023-07-04 21:23:40 -0500

    adding the functionality to the check answer method

--- Quiz/quiz_brain.py
+++ Quiz/quiz_brain.py
@@ -9,9 +9,14 @@ class QuizBrain:
         qq_text = self.questions_list[self.question_number].text
         qq_answer = self.questions_list[self.question_number].answer
         user_answer = input(f"Q.{self.question_number + 1}: {qq_text} (True/False)?: ")
+        self.check_answer(user_answer, qq_answer)
     
     def still_has_questions(self):
         return self.question_number < len(self.questions_list)
     
     def check_answer(self, user_answer, correct_answer):
-        pass
+        if user_answer.lower() == correct_answer.lower():
+            print("You got it right!")
+        else:
+            print("That's wrong.")
+            print(f"The correct answer was: {correct_answer}.")
