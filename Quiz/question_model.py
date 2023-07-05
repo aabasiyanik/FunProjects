354bc6ba9bb5fe3cb9a067a1ce87355d0d448970
Ahmed Abasiyanik <ahmedabasiyanik03@gmail.com>
2023-07-04 20:44:38 -0500

    creating constructor

--- Quiz/question_model.py
+++ Quiz/question_model.py
@@ -1,5 +1,16 @@
+from data import question_data
+import random
 class Question:
     def __init__(self, text, answer):
         self.text = text
         self.answer = answer
 
+arr = []
+random_num = random.randint(0,11)
+while random_num in arr:
+    random_num = random.randint(0,11)
+arr.append(random_num)
+question = question_data.pop(random_num)
+
+new_question = Question(text=question['text'], answer=question['answer'])
+print(new_question.answer)
