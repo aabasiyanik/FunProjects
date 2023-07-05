a47b9bf9559d4db887a225549674a9b5c4955b46
Ahmed Abasiyanik <ahmedabasiyanik03@gmail.com>
2023-07-04 21:02:18 -0500

    baddest ^_^ {added all the qustions in the question_data to a new_question array while creating their Question object by initializing Question object

---
+++ Quiz/main.py
@@ -0,0 +1,10 @@
+from data import question_data
+from question_model import Question
+
+new_question = []
+for question in question_data:
+    q_text = question['text']
+    q_answer = question['answer']
+    
+    new_question.append(Question(q_text, q_answer))
+
--- Quiz/question_model.py
+++ Quiz/question_model.py
@@ -4,13 +4,3 @@ class Question:
     def __init__(self, text, answer):
         self.text = text
         self.answer = answer
-
-arr = []
-random_num = random.randint(0,11)
-while random_num in arr:
-    random_num = random.randint(0,11)
-arr.append(random_num)
-question = question_data.pop(random_num)
-
-new_question = Question(text=question['text'], answer=question['answer'])
-print(new_question.answer)
