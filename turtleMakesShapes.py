921b507a3dadde91196ff35ce158a3749cf4ed38
Ahmed Abasiyanik <ahmedabasiyanik03@gmail.com>
2023-07-13 15:12:49 -0500

    change

---
+++ turtleMakesShapes.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+import random
+turtle = Turtle()
+
+colors = ['red', 'blue', 'green', 'yellow', 'orange', 'purple', 'pink', 'brown', 'black', 'aqua']
+
+for i in range(3,10):
+    random.shuffle(colors)
+    for _ in range(i):
+        turtle.pencolor(colors[0])
+        turtle.forward(100)
+        angle = 360 / i
+        turtle.right(angle)
+
+ws = Screen()
+ws.exitonclick()
